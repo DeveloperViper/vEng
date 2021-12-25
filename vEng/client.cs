8c75185ba08802a3c48cd0d78973aa2fe8ef94f9
fatbo <fatbo@DESKTOP-DS7HSU7>
2021-12-24 23:13:09 -0500

    Add project files.

---
+++ vEng/client.cs
@@ -0,0 +1,37 @@
+﻿using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENG
+{
+    public class client : BaseScript
+    {
+        [Command("eng")]
+        public void EngCommand(int source, List<object> args, string raw)
+        {
+            TriggerEvent("chat:addMessage", new
+            {
+                color = new[] { 0, 255, 0 },
+                multiline = true,
+                args = new[] { "Your Engine Has Now Been Toggled On/Off" }
+            });
+
+            var ped = PlayerPedId();
+            var vehicle = GetVehiclePedIsIn(ped, false);
+            SetVehicleEngineOn(vehicle, false, true, true);
+            var IsEngineRunning = GetIsVehicleEngineRunning(vehicle);
+            if (IsEngineRunning)
+            {
+                SetVehicleEngineOn(vehicle, true, true, true);
+
+
+
+            }
+            else SetVehicleEngineOn(vehicle, false, true, false);
+        }
+    }
+}
